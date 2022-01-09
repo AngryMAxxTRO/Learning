63a7ae826c9af2282927735b703c1827c494f310
Alex Bond <nfs2008@i.ua>
2022-01-09 17:47:29 +0200

    Mass

--- weight.py
+++ weight.py
@@ -1,10 +1,9 @@
 import sys
 def weight():
     print("Enter mass")
-    mass = int(sys())
-    massp = int(sys())
+    mass = int(sys.stdin.readline())
+    massp = float(sys.stdin.readline())
     for i in range(15):
         mass = mass + massp
-        print(round(mass * 0.165, 1))
-        print(mass, massp)
+        print(round(mass * 0.165, 2))
 weight()
